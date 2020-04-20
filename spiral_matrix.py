faeeb58f4ba80d2b2608c2ed886fe1fc54f22611
Max Belushkin <belushkin@gmail.com>
2020-04-20 13:02:42 +0300

    Added last technical task

--- spiral_matrix.py
+++ spiral_matrix.py
@@ -3,16 +3,17 @@ import sys
 
 
 class Solution:
-    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+    def spiralOrder(self, matrix: List[List[int]]):  # -> List[int]
         dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         size = len(matrix) * len(matrix[0]) if len(matrix) > 0 else 0
         pos = 0
         position = (0, 0)
-        output = []
+        # output = []
 
         for i in range(size):
             y, x = position
-            output.append(matrix[y][x])
+            # output.append(matrix[y][x])
+            print((y, x))
             matrix[y][x] = sys.maxsize
             if 0 <= x + dirs[pos][1] < len(matrix[0]) and 0 <= y + dirs[pos][0] < len(matrix) and \
                     matrix[y + dirs[pos][0]][x + dirs[pos][1]] != sys.maxsize:
@@ -21,11 +22,14 @@ class Solution:
                 pos = pos + 1 if pos + 1 < len(dirs) else 0
                 position = (y + dirs[pos][0], x + dirs[pos][1])
 
-        return output
+        # return output
 
+
+w, h = sys.argv[1].split('x')
+matrix = [[0 for x in range(int(w))] for y in range(int(h))]
 
 solution = Solution()
-
+solution.spiralOrder(matrix)
 
 # print(solution.spiralOrder([
 #     [1, 2, 3],
